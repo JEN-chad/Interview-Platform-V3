cb78e8faceeabe0d21d6c3fc1e7ec31309e67db4
JEN-chad <jenishj436@gmail.com>
2025-08-14 03:09:19 +0530

    Third patch

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -225,9 +225,6 @@ const Agent = ({
     setCallStatus(CallStatus.CONNECTING);
     if (type === "generate") {
       await vapi.start(
-        undefined,
-        undefined,
-        undefined,
         process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!,
         {
           variableValues: {
